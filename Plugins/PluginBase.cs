09c841cbc9ba6bb37d8b2a2e05d40831fcc8403e
Sukasa <nick.m@shaw.ca>
2016-03-09 12:54:08 -0800

    Refactor plugin settings and change IPlugin interface to base class

---
+++ Plugins/PluginBase.cs
@@ -0,0 +1,34 @@
+﻿using STOMP;
+using STOMP.Frames;
+using STOMP.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOMP.Server.Plugins
+{
+    public abstract class PluginBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="GrantedPermissions">
+        ///     The permissions the plugin has.  Used to return the permissions needed, if the plugin cannot load
+        /// </param>
+        ///     TRUE if the plugin can be loaded with the given permissions
+        /// <returns></returns>
+        public abstract bool Load(ref PluginInterface.PluginPermissions[] GrantedPermissions);
+
+        // TODO fill in delegate definitions better
+        public delegate void QueryConnectionCancelHook();
+        public delegate void ConnectionSuccessHook();
+        public delegate void ConnectionFailureHook();
+
+        public delegate void MessageNotificationHook();
+        public delegate StompFrame MessageModificationHook();
+
+        
+    }
+}
---
+++ Plugins/PluginSettings.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace STOMP.Server.Plugins
+{
+    public class PluginSettings
+    {
+        public string FriendlyName;
+        public string QualifiedName;
+        [JsonIgnore()] 
+        public string FileName;
+        [JsonIgnore()]
+        public AppDomain Domain;
+        public bool Loaded;
+        public object PluginCustomSettings;
+
+        public PluginInterface.PluginPermissions[] ActivePermissions;
+        public IEnumerable<Regex> MessageSubscriptions;
+
+        public PluginSettings(Assembly PluginAssembly, PluginSettings DefaultConfig)
+        {
+            AssemblyName Name = new AssemblyName(PluginAssembly.FullName);
+
+            FriendlyName = Name.Name;
+            QualifiedName = Name.FullName;
+
+            ActivePermissions = new PluginInterface.PluginPermissions[DefaultConfig.ActivePermissions.Length];
+            DefaultConfig.ActivePermissions.CopyTo(ActivePermissions, 0);
+
+            Loaded = false;
+            MessageSubscriptions = new List<Regex>();
+
+            FileName = PluginAssembly.Location;
+        }
+
+        public PluginSettings()
+        {
+            
+        }
+
+        internal static PluginSettings Default()
+        {
+            PluginSettings PS = new PluginSettings();
+
+            PS.ActivePermissions = new PluginInterface.PluginPermissions[] { PluginInterface.PluginPermissions.ReceiveMessage, PluginInterface.PluginPermissions.SendMessage };
+            
+            return PS;
+        }
+    }
+}
