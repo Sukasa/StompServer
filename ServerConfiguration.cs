4b93a40b7c81d1264ca3be40720c565480347ec7
Sukasa <nick.m@shaw.ca>
2016-03-09 13:14:18 -0800

    Add partial server config class

---
+++ ServerConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using STOMP.Server.Plugins;
+using System.Reflection;
+
+namespace STOMP.Server
+{
+    class ServerConfiguration
+    {
+        public IEnumerable<PluginSettings> PluginConfigurations;
+        public PluginSettings DefaultConfig = PluginSettings.Default();
+        public IDictionary<Assembly, PluginSettings> ConfigByCaller;
+
+        public void CreatePluginConfiguration(Assembly Plugin) {
+
+        }
+
+        public ServerConfiguration()
+        {
+            ConfigByCaller = new Dictionary<Assembly, PluginSettings>();
+            PluginConfigurations = ConfigByCaller.Values;
+        }
+    }
+}
